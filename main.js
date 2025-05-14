9c68a24d1cdd2469ae05028ea6a943aa6d03a5ee
kudratovazizbek2008 <kudratovazizbek2008@gmail.com>
2025-05-13 20:42:34 -0700

    creat lesson1

--- main.js
+++ main.js
@@ -1,37 +1,103 @@
 // 1-masala
-// let a = 12;
-// let b = 43;
-// let c = 32;
-//
-// let result = Math.max(a, b, c);
-//
-// console.log('eng katta son', result);
+// function main(a, b) {
+//   return a + b;
+// }
+// console.log(main(5, 3));
 
+
+
+
+
+// 2-masala
+// const a = (ism) => {
+//   return `Salom, ${ism}`;
+// }
+// console.log(a("Azizbek"));
 
 
 // 3-masala
-// let matn = "salom";
-//
-// for (let i = 0; i < matn.length; i++) {
-//  console.log(matn[i]);
+// function main(ball) {
+//   if (ball >= 90) {
+//     return "A";
+//   } else if (ball >= 80) {
+//     return "B";
+//   } else if (ball >= 70) {
+//     return "C";
+//   } else {
+//     return "F";
+//   }
+// }
+// console.log(main(85));
+
+// // 4-masala
+// let div = document.createElement("div");
+// div.innerText = "Salom dunyo";
+// document.body.appendChild(div);
+
+
+// 5-masala
+// function salom() {
+//   alert("Salom!");
 // }
 
 
-
-
-
-
-// 4-masla
-// let son = 456;
-// let matn = son.toString();
-// let yigindi = 0;
+// 7-masala
+// let number = [1, 2, 3];
 //
-// for (let i = 0; i < matn.length; i++) {
-//   yigindi += Number(matn[i]);
+// number.forEach(function(son) {
+//   console.log(son);
+// });
+
+
+// 8-masala
+// let a = [10, 20, 30];
+// a.splice(2, 3);
+// console.log(a);
+
+
+// 9-masala
+
+// let a = document.querySelector('img')
+//
+// a.addEventListener('click', event => {
+//   console.log("salom dunyo");
+// })
+
+
+// 10-masala
+
+// let a = [1, 2, 3, 4, 5, 6];
+//
+// let juft = a.filter(function(son) {
+//   return son % 2 === 0;
+// });
+//
+// console.log(juft);
+
+// 11-masala
+// let a = 10;
+// let b = 25;
+// let c = 15;
+//
+// let engKatta = (a > b && a > c) ? a : (b > c) ? b : c;
+//
+// console.log("Eng katta son:", engKatta);
+
+// 12-masala
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i);
+//   }
 // }
-//
-// console.log("Yig'indisi:", yigindi);
 
-// 6-masala
-// let ism = "Azizbek";
-// console.log("Salom" + ism + " ");
+
+// 14-masala
+
+// let name = ["Ali", "Vali", "Sara", "Diyor"];
+//
+// let a = name.map(function(ism) {
+//   return "Salom, " + ism + "!";
+// });
+//
+// console.log(a);
+

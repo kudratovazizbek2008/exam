3077c9c0db9bac409b4b6ff7d4bbc399e3282b86
kudratovazizbek2008 <kudratovazizbek2008@gmail.com>
2025-05-16 00:41:39 -0700

    creat lesson1

--- main.js
+++ main.js
@@ -1,103 +1,39 @@
-// 1-masala
-// function main(a, b) {
-//   return a + b;
-// }
-// console.log(main(5, 3));
 
 
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+const input = document.getElementById('guessInput');
+const button = document.getElementById('checkButton');
+const message = document.getElementById('message');
 
+button.addEventListener('click', () => {
+  const value = input.value;
 
+  if (value === '') {
+    alert('Iltimos, son kiriting!');
+    return;
+  }
 
-// 2-masala
-// const a = (ism) => {
-//   return `Salom, ${ism}`;
-// }
-// console.log(a("Azizbek"));
+  const guess = Number(value);
 
+  if (isNaN(guess)) {
+    alert('Faqat raqam kiriting!');
+    return;
+  }
 
-// 3-masala
-// function main(ball) {
-//   if (ball >= 90) {
-//     return "A";
-//   } else if (ball >= 80) {
-//     return "B";
-//   } else if (ball >= 70) {
-//     return "C";
-//   } else {
-//     return "F";
-//   }
-// }
-// console.log(main(85));
-
-// // 4-masala
-// let div = document.createElement("div");
-// div.innerText = "Salom dunyo";
-// document.body.appendChild(div);
-
-
-// 5-masala
-// function salom() {
-//   alert("Salom!");
-// }
-
-
-// 7-masala
-// let number = [1, 2, 3];
-//
-// number.forEach(function(son) {
-//   console.log(son);
-// });
-
-
-// 8-masala
-// let a = [10, 20, 30];
-// a.splice(2, 3);
-// console.log(a);
-
-
-// 9-masala
-
-// let a = document.querySelector('img')
-//
-// a.addEventListener('click', event => {
-//   console.log("salom dunyo");
-// })
-
-
-// 10-masala
-
-// let a = [1, 2, 3, 4, 5, 6];
-//
-// let juft = a.filter(function(son) {
-//   return son % 2 === 0;
-// });
-//
-// console.log(juft);
-
-// 11-masala
-// let a = 10;
-// let b = 25;
-// let c = 15;
-//
-// let engKatta = (a > b && a > c) ? a : (b > c) ? b : c;
-//
-// console.log("Eng katta son:", engKatta);
-
-// 12-masala
-// for (let i = 1; i <= 100; i++) {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     console.log(i);
-//   }
-// }
-
-
-// 14-masala
-
-// let name = ["Ali", "Vali", "Sara", "Diyor"];
-//
-// let a = name.map(function(ism) {
-//   return "Salom, " + ism + "!";
-// });
-//
-// console.log(a);
-
+  if (guess === randomNumber) {
+    input.style.backgroundColor = '#d4edda';
+    input.style.border = '2px solid green';
+    message.textContent = 'Tabriklaymiz! To‘g‘ri javob!';
+    console.log('To‘g‘ri javob:', randomNumber);
+  } else if (guess < randomNumber) {
+    input.style.backgroundColor = '#f8d7da';
+    input.style.border = '2px solid red';
+    message.textContent = 'Notug`ri javob: Kattaroq son kiriting';
+    console.log('To‘g‘ri javob:', randomNumber);
+  } else {
+    input.style.backgroundColor = '#f8d7da';
+    input.style.border = '2px solid red';
+    message.textContent = 'Notug`ri javob: Kichikroq son kiriting';
+    console.log('To‘g‘ri javob:', randomNumber);
+  }
+});
